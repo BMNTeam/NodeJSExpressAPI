b63a01d9f78bfef44ebf376db21b44f67bc48aa0
BMNTeam <maksim_bender08@mail.ru>
2017-12-15 17:24:55 +0300

    First Version of API

---
+++ api/routes/products.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
+router.get('/', (req, res, next) => {
+  Product.find()
+      .exec()
+      .then(docs => {
+        console.log(docs);
+        if( docs.length >= 0) {
+          res.status(200).json(docs);
+        } else {
+          res.status(404).json({
+            message: 'No entries found'
+          })
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+});
+
+router.post('/', (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product.save()
+      .then((result) => {
+        console.log(result)
+        res.status(201).json({
+          message: 'Handling a POST request to the /products',
+          createdProduct: result
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+
+});
+
+router.get('/:productId', (req, res, next) => {
+  "use strict";
+  const id = req.params.productId;
+  Product.findById(id)
+      .exec()
+      .then(doc => {
+        console.log(doc);
+        res.status(200).json({doc})
+      })
+      .catch(err => console.log(err))
+
+});
+
+
+router.patch('/:productId', (req, res, next) => {
+  "use strict";
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+
+  Product.update({_id: id}, {$set: updateOps })
+      .exec()
+      .then(result => {
+        console.log(result);
+        res.status(200).json({result});
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+
+});
+
+router.delete('/:productId', (req, res, next) => {
+  "use strict";
+  const id = req.params.productId;
+  Product.remove({_id: id})
+      .exec()
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  res.status(200).json({
+    message: 'Delete product'
+  })
+
+});
+
+
+module.exports = router;
