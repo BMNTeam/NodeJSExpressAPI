bf990cc7f02065a69f7218536bde817f0258427e
BMNTeam <maksim_bender08@mail.ru>
2017-12-26 22:15:33 +0300

    Now users can upload images

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,40 +1,121 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   "use strict";
-  res.status(200).json({
-    message: 'Order were fetched'
-  })
+  Order.find()
+      .populate('product', 'name price')
+      .exec()
+      .then( (docs) => {
+        res.status(200).json({
+          count: docs.length,
+          orders: docs.map((doc) => {
+            return {
+              id: doc._id,
+              product: doc.product,
+              quantity: doc.quantity,
+              request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders/' + doc._id
+              }
+            }
+          }),
+
+        })
+      })
+      .catch( (err) => {
+        res.status(500).json({
+          error: err
+        })
+      })
 });
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  };
-  "use strict";
-  res.status(201).json({
-    message: 'Order has caught',
-    order: order
-  })
+  Product.findById(req.body.productId)
+      .then( product => {
+        if(!product) {
+          return res.status(404).json({
+            message: 'Product not found',
+
+          })
+        }
+        const order = new Order({
+          _id: mongoose.Types.ObjectId(),
+          quantity: req.body.quantity,
+          product: req.body.productId
+        });
+        return order
+            .save()
+
+      })
+      .then( result => {
+        console.dir(result);
+        res.status(201).json({
+          message: 'Order Stored',
+          createdProduct: {
+            _id: result.id,
+            product: result.product,
+            quantity: result.quantity
+          },
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/orders/' + result._id
+          }
+        })
+      })
+      .catch( err => {
+        console.dir(err);
+        res.status(500).json({
+          message: 'Product does not exists',
+          error: err
+        })
+      });
+
 });
 
 router.get('/:orderId', (req, res, next) => {
-  "use strict";
-  res.status(200).json({
-    message: 'Here is your ordrer',
-    orderId: req.params.orderId
-  })
+  Order.findById(req.params.orderId)
+      .populate('product', 'name price')
+      .exec()
+      .then(order => {
+        res.status(200).json({
+          order,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/orders/'
+          }
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        })
+      })
 
 });
 
 router.delete('/:orderId', (req, res, next) => {
-  "use strict";
-  res.status(200).json({
-    message: 'You delete this order',
-    orderId: req.params.orderId
-  })
+  Order.remove(req.params.orderId)
+      .exec()
+      .then(result => {
+       res.status(200).json({
+         message: 'Order deleted',
+         request: {
+           type: 'POST',
+           url: 'http://localhost:3000/orders',
+           body: {productId: "ID", quantity: 'Number'}
+         }
+       })
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        })
+      })
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+});
+
+
+
+const fileFilter = (req, file, cb) => {
+  //reject file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(new Error('not matched type'), false)
+  }
+};
+
+const upload = multer(
+    {
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3
+      },
+      fileFilter: fileFilter
+    });
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   Product.find()
-      .select('name price _id')
+      .select('name price _id productImage')
       .exec()
       .then(docs => {
         console.log(docs);
@@ -18,6 +48,7 @@ router.get('/', (req, res, next) => {
                 name: doc.name,
                 price: doc.price,
                 _id: doc._id,
+                productImage: doc.productImage,
                 request: {
                   type: 'GET',
                   url: 'http://localhost:3000/products/' + doc._id
@@ -39,11 +70,13 @@ router.get('/', (req, res, next) => {
 
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'),(req, res, next) => {
+  console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product.save()
       .then((result) => {
@@ -62,7 +95,10 @@ router.post('/', (req, res, next) => {
         })
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
+        res.status(500).json({
+          error: err
+        })
       });
 
 });
@@ -71,7 +107,7 @@ router.get('/:productId', (req, res, next) => {
   "use strict";
   const id = req.params.productId;
   Product.findById(id)
-      .select('name price _id')
+      .select('name price _id productImage')
       .exec()
       .then(doc => {
         console.log(doc);
@@ -112,6 +148,9 @@ router.patch('/:productId', (req, res, next) => {
       })
       .catch(err => {
         console.log(err);
+        res.status(500).json({
+          error: err
+        })
       });
 
 
