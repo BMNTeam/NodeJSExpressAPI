3150fc0e66d682de642fbab9ec41f25d8e7a5635
BMNTeam <maksim_bender08@mail.ru>
2018-01-25 23:53:21 +0300

    User signIn and SignUp routes added and also added token generation for signIn and SignUp

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user')
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
+app.use('/user', userRoutes);
 
 mongoose.connect('mongodb://localhost:27017/apiexpress', {
   useMongoClient: true
